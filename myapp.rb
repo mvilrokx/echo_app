d24fcf2c565e4a10ad535ad2c306b6906135ed6b
Mark Vilrokx <mvilrokx@gmail.com>
2012-03-15 23:50:51 -0700

    added json support

--- myapp.rb
+++ myapp.rb
@@ -1,10 +1,19 @@
 # myapp.rb
 require 'sinatra'
+require 'sinatra/respond_to'
+require 'json'
+require 'haml'
+
+Sinatra::Application.register Sinatra::RespondTo
 
 get '/' do
   'Hello world!'
 end
 
 get '/hello/:name' do
-  "Hello #{params[:name]}!"
+  return_value = {:name => params[:name]}
+  respond_to do |wants|
+    wants.html { haml :hello, :locals => return_value }
+    wants.json { return_value.to_json }
+  end
 end
